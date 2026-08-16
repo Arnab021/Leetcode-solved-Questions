43d0c5bc312ca11f65c3a46984d64e40fc4cd64f
ARNAB MAJEE <149416423+Arnab021@users.noreply.github.com>
2026-08-16 11:49:38 +0530

    Time: 8 ms (30.2%), Space: 131.3 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int n=stones.size();
+        int count0=0;
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<n;i++) {
+            if(stones[i]%3==0) {
+                count0++;
+            }
+            else if(stones[i]%3==1) {
+                count1++;
+            }
+            else {
+                count2++;
+            }
+        }
+        if(count0%2==0) {
+            if(count1>0&&count2>0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            if(count1>count2+2||count2>count1+2) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+};
