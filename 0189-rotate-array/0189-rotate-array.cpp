05c7fb81e77f0940dc50e7fff9bd69dd174afeea
ARNAB MAJEE <149416423+Arnab021@users.noreply.github.com>
2026-08-20 22:59:35 +0530

    Time: 0 ms (100%), Space: 262.7 MB (13.32%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,15 +3,11 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         vector<int> arr1;
-        vector<int> arr2;
         k=k%n;
-        for(int i=0;i<n-k;i++){
-            arr1.push_back(nums[i]);
-        } 
         for(int i=n-k;i<n;i++){
-            arr2.push_back(nums[i]);
+            arr1.push_back(nums[i]);
         }
-        arr2.insert(arr2.end(),arr1.begin(),arr1.end());
-        nums=arr2;
+        nums.erase(nums.end()-k,nums.end());
+        nums.insert(nums.begin(),arr1.begin(),arr1.end());
     }
 };
