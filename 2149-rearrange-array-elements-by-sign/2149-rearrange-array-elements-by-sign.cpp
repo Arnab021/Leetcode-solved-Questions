6964bc70999132e1fd714dc74d31657bfc777608
ARNAB MAJEE <149416423+Arnab021@users.noreply.github.com>
2026-08-25 20:55:42 +0530

    Time: 2 ms (88.05%), Space: 128.4 MB (54.16%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector <int> ans(n);
+        int l = 0,r = 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                ans[l] = nums[i];
+                l += 2;
+            } else {
+                ans[r] = nums[i];
+                r += 2;
+            }
+        }
+        return ans;
+    }
+};
